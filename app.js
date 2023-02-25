2e471a9ccd68f58b37051122f2d6459919414447
soonable@naver.com <soonable@naver.com>
2023-02-26 02:27:04 +0900

    feat : conditionals

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-const calculator = {
-    plus: function (a, b) {
-        return (a + b);
-    },
-    minus: function (a, b) {
-        return (a - b);
-    },
-    divide: function (a, b) {
-        return (a / b);
-    },
-    times: function (a, b) {
-        return (a * b);
-    },
-    pow: function (a, b) {
-        return (a ** b);
-    }
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(4, 5);
-const minusResult = calculator.minus(plusResult, 2);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powResult = calculator.pow(divideResult, minusResult);
+console.log(isNaN(age));
+if(isNaN(age) || age < 0) {
+    console.log("Please write a real positive number");
+} else if (age < 18) {
+    console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+    console.log("You can drink");
+} else {
+    console.log("Careful!");
+}
