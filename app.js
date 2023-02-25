fc57a33b8a1dc9a1b9409bf80aad18cdce1c4ba3
soonable@naver.com <soonable@naver.com>
2023-02-26 01:20:11 +0900

    feat : returns

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const calculator = {
-    add: function(a,b) {
-        console.log(a+b);
+    plus: function (a, b) {
+        return (a + b);
     },
-    minus: function(a,b) {
-        console.log(a-b);
+    minus: function (a, b) {
+        return (a - b);
     },
-    divide: function(a,b) {
-        console.log(a/b);
+    divide: function (a, b) {
+        return (a / b);
     },
-    multiply: function(a,b){
-        console.log(a*b);
+    times: function (a, b) {
+        return (a * b);
     },
-    pow: function(a,b) {
-        console.log(a**b);
+    pow: function (a, b) {
+        return (a ** b);
     }
 };
 
-calculator.pow(2,8);
+const plusResult = calculator.plus(4, 5);
+const minusResult = calculator.minus(plusResult, 2);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powResult = calculator.pow(divideResult, minusResult);
