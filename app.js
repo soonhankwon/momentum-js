107198c2c0c5332543edb03fb88dbb319257786b
soonable@naver.com <soonable@naver.com>
2023-02-26 02:29:02 +0900

    feat : conditionals

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ if(isNaN(age) || age < 0) {
     console.log("You are too young");
 } else if (age >= 18 && age <= 50) {
     console.log("You can drink");
+} else if (age === 100) {
+    console.log("OMG");
 } else {
     console.log("Careful!");
 }
