4428421778d39acc9f8acb05c93bf4d598bdda10
soonable@naver.com <soonable@naver.com>
2023-02-28 00:38:33 +0900

    feat : events

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const title = document.querySelector(".hello h1");
-title.innerText = "Whatup!";
-console.log(title);
+const title = document.querySelector(".hello:first-child h1");
+console.dir(title);
+
+function handleTitleClick() {
+    title.style.color= "red";
+    console.log("title was clicked!");
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
