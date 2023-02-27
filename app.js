bd46c45c5ff8d940a209abc413560bf2e16b60dc
soonable@naver.com <soonable@naver.com>
2023-02-27 23:36:28 +0900

    feat : searching for elements

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-const age = parseInt(prompt("How old are you?"));
-
-console.log(isNaN(age));
-if(isNaN(age) || age < 0) {
-    console.log("Please write a real positive number");
-} else if (age < 18) {
-    console.log("You are too young");
-} else if (age >= 18 && age <= 50) {
-    console.log("You can drink");
-} else if (age === 100) {
-    console.log("OMG");
-} else {
-    console.log("Careful!");
-}
+const title = document.querySelector(".hello h1");
+title.innerText = "Whatup!";
+console.log(title);
