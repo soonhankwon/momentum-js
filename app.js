35b925c7e5198cfd2ad9a93ad6d5327fa935f0f4
soonable@naver.com <soonable@naver.com>
2023-03-02 23:00:53 +0900

    feat : toggle

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-    h1.style.color= "red";
-    console.log("title was clicked!");
-}
-
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("Online!")
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
