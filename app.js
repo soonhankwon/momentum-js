fbff2e59968fc6566b3121ae951d66ba2d2d51ad
soonable@naver.com <soonable@naver.com>
2023-03-06 15:15:54 +0900

    loading username

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username} keep going!`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
 function handleLinkClick(event) {
@@ -18,3 +18,19 @@ function handleLinkClick(event) {
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUsername);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
